868af33792cd0a6752ad63b555b66b7cd27d18cd
bernardogolbert <bernardogolbertdeoliveira@gmial.com>
2026-03-09 18:41:26 -0300

    created 'New Grid' button and adjusted the grid generation logic

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const container = document.querySelector("#container-grid");
 
-for (let i = 0; i < 256; i++) {
-  const divs = document.createElement("div");
-  divs.classList.add("grid");
-  container.appendChild(divs);
-  divs.addEventListener('mouseover', () =>{
-    divs.style.backgroundColor = 'blue'
-  });
+let valor = 256;
+
+const btn = document.createElement("button");
+btn.classList.add("btn");
+document.body.appendChild(btn);
+btn.textContent = "New Grid";
+btn.addEventListener("click", () => {
+  valor = parseInt(prompt("Grid Dimension"));
+  container.innerHTML('')
+  blocks(valor)
+});
+
+function blocks(int) {
+  for (let i = 0; i < int; i++) {
+    const divs = document.createElement("div");
+    divs.classList.add("grid");
+    container.appendChild(divs);
+    divs.addEventListener("mouseover", () => {
+      divs.style.backgroundColor = "blue";
+    });
+  }
 }
