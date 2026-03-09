0e183adb73539e68d1597182479151f201c29ea2
bernardogolbert <bernardogolbertdeoliveira@gmial.com>
2026-03-09 17:46:11 -0300

    implemented hovering function

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const container = document.querySelector('#container-grid')
-
+const container = document.querySelector("#container-grid");
 
 for (let i = 0; i < 256; i++) {
-    const divs = document.createElement("div")
-    divs.classList.add('grid')
-    container.appendChild(divs);    
-};
-
+  const divs = document.createElement("div");
+  divs.classList.add("grid");
+  container.appendChild(divs);
+  divs.addEventListener('mouseover', () =>{
+    divs.style.backgroundColor = 'blue'
+  });
+}
